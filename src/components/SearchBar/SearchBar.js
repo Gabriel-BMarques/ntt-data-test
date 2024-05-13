ef234d8948e309b00d86fddb491bbfcb7e5cc528
Gabriel-BMarques <gabrielbutkus256@gmail.com>
2024-05-12 23:02:02 -0300

    feat: implemented moviesService

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,23 @@
+// movieService.js
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:4000',
+});
+
+const searchMovies = async (query) => {
+  try {
+    const response = await axiosInstance.get('/', {
+      params: {
+        title: query,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error('Error searching movies:', error);
+    return [];
+  }
+};
+
+export { searchMovies };
