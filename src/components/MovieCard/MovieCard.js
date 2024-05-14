ad1631e98dcbd4455b0073526abcc44587eea170
Gabriel-BMarques <gabrielbutkus256@gmail.com>
2024-05-13 22:52:40 -0300

    feat: added favorite button

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -5,6 +5,8 @@ import '@ui5/webcomponents/dist/Table';
 import '@ui5/webcomponents/dist/TableColumn';
 import './MovieCard.scss';
 import RatingStars from '../RatingStars/RatingStars';
+import Button from '../Button/Button';
+import { FaRegHeart } from "react-icons/fa";
 
 function MovieCard({ title, plot, actors, poster, ratings }) {
     return (
@@ -15,6 +17,11 @@ function MovieCard({ title, plot, actors, poster, ratings }) {
                     <p>{ plot }</p>
                     <p><b>Actors:</b> { actors }</p>
                     <p><b>Review:</b> <RatingStars ratings={ratings} /></p>
+                    <Button>
+                        <span className='favorite-button-label'>
+                            Favorite&nbsp;<FaRegHeart />
+                        </span>
+                    </Button>
                 </ui5-table-column>
                 <ui5-table-column slot='columns'>
                     <img src={ poster } alt='Movie Poster' />
